fc9991c496da8507e5261a624267d71f3f5e22a4
Alvin Abia <alvin.abia@icloud.com>
2024-07-09 16:06:16 -0700

    chore: initial commit

---
+++ src/main/java/com/alvinAbia/designPatternTermProject/DesignPatternTermProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alvinAbia.designPatternTermProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternTermProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternTermProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvinAbia/designPatternTermProject/DesignPatternTermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvinAbia.designPatternTermProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternTermProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
